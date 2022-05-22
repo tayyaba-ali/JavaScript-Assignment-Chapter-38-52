a097966af540e9ffe6a9376bd2d811c443a88bbf
Tayyaba-Ali20 <ta6402792@gmail.com>
2022-05-22 16:40:34 +1100

    added

--- chap 38-42.js
+++ chap 38-42.js
@@ -5,7 +5,7 @@
 
 // function power(num,power){
 //       var power=num**power;
-//       return power        
+//       return power
 // }
 
 // var a=+prompt('Enter number ');
@@ -128,3 +128,108 @@
 // var result = removeVowels(str);
 // document.write('Before :' + str + '<br>');
 // document.write('After  : ' + result + '<br>');
+
+
+// Task 7
+// Write a function with switch statement to count the number of occurrences of any two vowels in succession in a line of text.
+
+
+// var str = 'Please read this application and give me gratuity';
+// var occurence = [];
+// var count = 0;
+// function findOccurence() {
+//     var chars = str.toLowerCase().split('');
+//     for (var i = 0; i < chars.length; i++) {
+//         var char = chars[i];
+//         var next = chars[i + 1];
+//         if (isCorrectCharc(char) && isCorrectCharc(next)) {
+//             count++;
+//             var twoChar = char + next;
+//             occurence.push(twoChar)
+//         }
+//     } return count
+// }
+// function isCorrectCharc(char) {
+//     switch (char) {
+//         case 'a':
+//         case 'e':
+//         case 'i':
+//         case 'o':
+//         case 'u':
+//             return true;
+//         default:
+//             return false
+//     }
+
+// }
+// var count = findOccurence();
+// document.write('String :' + str + '<br>');
+// document.write('Count :' + count + '<br>');
+// document.write('Occurence :' + occurence + '<br>');
+
+// Task 8
+// The distance between two cities (in km.) is input through the keyboard. Write four functions to convert and print this distance in meters, feet, inches and centimeters
+
+// var distance = +prompt("Enter distance from city A to City B in KM: ")
+// meter(distance)
+// feet(distance)
+// inches(distance)
+// centimeters(distance)
+
+// function meter(dis){
+//   var meterDistance = dis * 1000
+//   document.write("Distance between city A to city B is " + meterDistance + " meters." + "<br>")
+// }
+
+// function feet(dis){
+//   var feetDistance = dis * 3281
+//   document.write("Distance between city A to city B is " + feetDistance + " feets." + "<br>")
+// }
+
+// function inches(dis){
+//   var inchDistance = dis * 39370
+//   document.write("Distance between city A to city B is " + inchDistance + " inches." + "<br>")
+// }
+
+// function centimeters(dis){
+//   var centimeterDistance = dis * 100000
+//   document.write("Distance between city A to city B is " + centimeterDistance + " centimeters." + "<br>")
+// }
+
+
+// Task 9
+// Write a program to calculate overtime pay of employees.Overtime is paid at the rate of Rs. 12.00 per hour for every hour worked above 40 hours. Assume that employees do not work for fractional part of an hour.
+
+// var workingHours = +prompt("Enter your working hours: ")
+// var overTime, overTimePay;
+// var overTimeHourPay = 12;
+
+// if(workingHours > 40){
+//   overTime = workingHours - 40;
+//   overTimePay = overTime * overTimeHourPay;
+//   alert("Your overtime pay cost is: " + overTimePay + "rupees");
+// }
+
+// else{
+//   alert("You need to work more to get overtime")
+// }
+
+// Task 10
+// A cashier has currency notes of denominations 10, 50 and 100. If the amount to be withdrawn is input through the keyboard in hundreds, find the total number of currency notes of each denomination the cashier will have to give to the withdrawer.
+
+// var amount = +prompt("Enter amount to Withdraw")
+// var hundred = parseInt(amount / 100)
+// var fifty = parseInt((amount % 100) / 50)
+// var ten = parseInt((((amount % 100) % 50) / 10))
+
+// document.write("Entered amount: " + amount + "<br>");
+// document.write("You will have " + hundred + " hundred notes " + fifty + " fifty notes " + ten + " ten notes.")
+
+
+//-------End of Chap 38-42
+
+
+
+
+
+
